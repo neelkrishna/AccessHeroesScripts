0bd44a3b4ed11cb382a1a69aa75939b5750be3f3
Neel Krishna <neelkri93@gmail.com>
2024-05-02 07:38:06 -0400

    working state with all requested features as of may 2

--- code/MakeWixCSVFromWappalyzerCSV.java
+++ code/MakeWixCSVFromWappalyzerCSV.java
@@ -37,10 +37,10 @@ public class MakeWixCSVFromWappalyzerCSV {
                     continue;
                 }
 
-                String parsedFirstName = parseFirstName(entry);
-                String parsedLastName = parseLastName(entry);
+                String parsedFirstName = toTitleCase(removeLeadingOrTrailingSpaces(parseFirstName(entry)));
+                String parsedLastName = toTitleCase(removeLeadingOrTrailingSpaces(parseLastName(entry)));
                 String parsedEmail = parseEmail(entry);
-                String parsedCompanyName = parseCompanyName(entry);
+                String parsedCompanyName = toTitleCase(removeLeadingOrTrailingSpaces(parseCompanyName(entry)));
                 String parsedURL = parseURL(entry);
 
                 // check to see if any of the fields collected make the entry invalid, as defined by logic in each function. Skips invalid entries
@@ -162,5 +162,40 @@ public class MakeWixCSVFromWappalyzerCSV {
  
         } 
     }
+
+    public static String toTitleCase(String s) {
+        if(s.length() < 2) {
+            return s.toUpperCase();
+        }
+        if(s.indexOf(" ") != -1) {
+            String output = "";
+            String[] words = s.split(" ");
+
+            for(String word: words) {
+                if(word.length() == 0) output += "";
+                else if(word.length() == 1) output += word.toUpperCase() + " ";
+                else {
+                    output += word.substring(0, 1).toUpperCase() + word.substring(1, word.length()).toLowerCase() + " ";
+                }
+            }
+            return output;
+        } else {
+            return s.substring(0, 1).toUpperCase() + s.substring(1, s.length()).toLowerCase() + " ";
+        }
+        
+    }
     
+    public static String removeLeadingOrTrailingSpaces(String s) {
+        if(s.length() < 2) return s;
+        else {
+            if(s.substring(0, 1).equals(" ") || s.substring(0, 1).equals("\"")) {
+                s = s.substring(1, s.length());
+            }
+            if(s.substring(s.length() - 1, s.length()).equals(" ") || s.substring(s.length() - 1, s.length()).equals("\"")) {
+                s = s.substring(0, s.length() - 1);
+            }
+            return s;
+        }
+        
+    }
 }
